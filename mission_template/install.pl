4469c10fbce2cd2c0202a5bfd5b5f834b585b336
pcb <name@address>
2021-07-01 08:47:23 -0700

    added creation of sendit.sh script

--- mission_template/install.pl
+++ mission_template/install.pl
@@ -1,5 +1,6 @@
 
 my $default_base_path = "/mnt/c/Documents\ and\ Settings/nazgh/My\ Documents/Arma\ 3/mpmissions/";
+my $default_pbo_path = "/mnt/d/SteamLibrary/steamapps/common/Arma\ 3/MPMissions/";
 
 if ($#ARGV < 0) {
     die "specify path on command line!\n\n\n";
@@ -32,6 +33,7 @@ if (! -e "$path/mission.sqm") {
 #
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+if (0) {
 print "copying .sqf files ...\n";
 print `cp *.sqf "$path"`;
 
@@ -44,5 +46,24 @@ print `cp -R functions "$path"`;
 print "copying missions ...\n";
 print `cp -R missions "$path"`;
 
+};
 
+# #################################################
+# make a "sendit.sh" script for uploading .pbo file
+# #################################################
+my @fields = split /\//, $path;
+my $name = $fields[$#fields] . ".pbo";
+my $pbo = $default_pbo_path . $name;
+
+if (! -e $pbo) {
+    die "Error! pbo file <$pbo> doesn't exist!\n\n";
+}
+
+open(FH, '>', "sendit.sh");
+print FH "#!\n";
+print FH "scp \"$pbo\" root\@94.26.31.102:ds_$name\n";
+close(FH);
+print `chmod a+x sendit.sh`;
+
+print "Created 'sendit.sh' file\n";
 
